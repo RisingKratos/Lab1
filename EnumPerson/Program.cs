ab72f635de12f45595113d69a3f3d9642c416ad2
RisingKratos <risingkratos@gmail.com>
2015-02-22 01:11:49 +0600

    Lab1

---
+++ EnumPerson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public enum Genders : int {Male, Female};
+    struct Person
+    {
+        public string name;
+        public string surName;
+        public int age;
+        public Genders gender;
+        public Person(string Name, string SurName, int Age,Genders Gender)
+        {
+            name = Name;
+            surName = SurName;
+            age = Age;
+            gender = Gender;
+        }
+        public override string ToString()
+        {
+            return name + ' ' + surName + ", age: " + age + ", sex: " + gender;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Muslim", "Beibytuly", 18, Genders.Male);
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    struct Person
+    {
+        public string name;
+        public string surName;
+        public int age;
+        public Person(string Name, string SurName, int Age)
+        {
+            name = Name;
+            surName = SurName;
+            age = Age;
+        }
+        public override string ToString()
+        {
+            return name + ' ' + surName + ", age: " + age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Muslim","Beibytuly",18);
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
